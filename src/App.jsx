081b916c1fe19b5278d35206ac210b6c5a048fbc
Vijay <axelblaze14022000@gmail.com>
2023-06-29 15:54:29 +0530

    Initial commit for NewsWave

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="text-center font-bold text-2xl font-serif py-3">
+          NewsWave - Catch the Latest Stories in Motion!
+        </div>
+        <p className="text-center font-serif px-52">
+          Stay informed and ride the wave of breaking news with NewsWave.
+          Discover the latest updates, trending topics, and captivating stories
+          from around the world. Our sleek and intuitive website delivers a
+          seamless browsing experience, keeping you at the forefront of what
+          matters most. Dive into a dynamic wave of information and stay
+          connected with NewsWave today.
+        </p>
+      </>
+    );
+  }
+}
