f1224c37b20f54b851f9e3685880dd1a53881103
Vijay <axelblaze14022000@gmail.com>
2023-07-06 12:17:07 +0530

    Added template articles and rendered with News Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import News from "./components/News";
 
 export default class App extends Component {
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <>
         <Navbar />
-        <News pageTitle="Top Headlines"/>
+        <News pageTitle="Top Headlines" />
       </>
     );
   }
