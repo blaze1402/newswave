fc346a35c0a2783800d93f832ee0ac0dd2ba12f7
Vijay <axelblaze14022000@gmail.com>
2023-07-06 19:08:01 +0530

    resolved a components path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import News from "./components/News";
 
 export default class App extends Component {
